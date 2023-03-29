7da952afb9a4a5f6fb095be34854d0b028e6d1bd
I-Viktor <Lafasolka@gmail.com>
2023-03-29 20:00:22 +0300

    final

--- Index.js
+++ Index.js
@@ -5,9 +5,42 @@ const tasks = [
     { task: 'breakfast' }
 ];
 
-function addNewTask{
+function addNewTask() {
+    const newInputTask = document.getElementById('new-task');
+    console.log('newInputTask', newInputTask.value);
+
+    const ul = document.querySelector('.list-group');
+    const li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.onclick = () => {
+        li.classList.add('active');
+    }
+
+
+
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-success');
+    button.innerHTML = 'DELETE';
+    button.onclick = () => {
+        li.remove();
+    }
+
+    tasks.unshift({ task: newInputTask.value })
+    console.log('tasks', tasks);
+    li.append(button, newInputTask.value);
+    ul.append(li)
+
+
 
 }
+
+
+const btnInput = document.querySelector('.btn-inpun');
+btnInput.addEventListener('click', addNewTask);
+
+
+
+
 
 function getAllTasks(allTasks) {
     const ul = document.querySelector('.list-group');
@@ -28,7 +61,7 @@ function getAllTasks(allTasks) {
             li.remove();
         }
         li.append(button, arrTask.task);
-        console.log('li', li)
+        // console.log('li', li)
         return li
 
     })
