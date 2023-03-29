3f1cae93c9e8d52da24393a26854804827751076
I-Viktor <Lafasolka@gmail.com>
2023-03-29 16:00:52 +0300

    add function getAllTasks

--- Index.js
+++ Index.js
@@ -3,4 +3,28 @@ const tasks = [
     { task: 'shoping' },
     { task: 'go to the gym' },
     { task: 'breakfast' }
-]
+];
+
+
+
+function getAllTasks(allTasks) {
+    const ul = document.querySelector('.list-group');
+
+    allTasks.map(() => {
+        const li = document.createElement('li');
+        li.classList.add('list-group-item');
+        li.append(task)
+        // console.log('li', li)
+        return li
+
+    })
+
+
+
+
+}
+getAllTasks(tasks)
+
+
+
+console.log('test')
